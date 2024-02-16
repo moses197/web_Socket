0bc43fbed081ea771448b7a9c5358b05cc4c3fa5
moses197 <leomoszeko@gmail.com>
2024-02-16 12:40:04 +0100

    added express framework to make it robust

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
-import { createServer } from 'http'
+import express from 'express'
 import { Server } from 'socket.io'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+// const httpServer = createServer()
+
+const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://127.0.0.1:3500","http://localhost:5500", "http://127.0.0.1:5500", ]
     }
 })
 
@@ -19,4 +34,4 @@ io.on('connection', socket => {
 })
 
 
-httpServer.listen(3500, () => console.log('Listening on port 3500'))
+// httpServer.listen(3500, () => console.log('Listening on port 3500'))
