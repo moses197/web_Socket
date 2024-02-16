793805d98bc85de9e7e1295e7fa4ad670cf63fc0
moses197 <leomoszeko@gmail.com>
2024-02-16 13:12:56 +0100

    added little functionalities like real-time typing notification and others

--- server/index.js
+++ server/index.js
@@ -25,11 +25,28 @@ const io = new Server(expressServer, {
 })
 
 io.on('connection', socket => {
-    console.log(`USer ${socket.id} connected`)
+    console.log(`User ${socket.id} connected`)
 
+    // Upon established connection - only to user
+    socket.emit('message', "Welcome to Chat APP")
+
+    // Upon established connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`) 
+
+    // Listening for a message event
     socket.on('message', data => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0,5)}: ${data} `)
+    })
+
+    // When user disconnects - broadcast to all others
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`) 
+    })
+
+    // Listening for activity
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })
 
